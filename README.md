9394ae878c286b6d65d12cd862684a5a6f0a1d7e
nnn1590 <nnn1590@nnn1590.org>
2020-05-20 05:00:08 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# twibot-py
+
+## Usage
+
+_It assumes the current directory is git root._
+```bash
+git clone https://github.com/nnn1590/twibot-py
+cd twibot-py
+```
+
+1. Copy `config.py.template` to `config.py`
+2. Rewriting `config.py` to suit you
+3. Run `__main__.py` (e.g. `python3 __main__.py` or `python3 .`)
---
+++ __main__.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+# REFERENCES: https://realpython.com/twitter-bot-python-tweepy/#how-to-make-a-twitter-bot-in-python-with-tweepy
+#             https://stackoverflow.com/questions/49731259/tweepy-get-tweets-between-two-dates
+#             https://stackoverflow.com/questions/39985434/python-tweepy-how-to-use-lookup-friendships
+# THANKS!
+
+import tweepy
+from config import *
+import time
+import datetime
+
+def has_firm_friendship(api, tw):
+    infomation = api.show_friendship(source_screen_name=MY_SCREEN_NAME, target_id=tw.user.id_str)
+    return True if (infomation[0].following and infomation[0].followed_by and infomation[1].following and infomation[1].followed_by) else False
+    return False
+
+def check_mentions(api, keywords, since_id):
+    print("Retrieving mentions")
+    new_since_id = since_id
+    for tweet in tweepy.Cursor(api.mentions_timeline,
+        since_id=since_id).items():
+        new_since_id = max(tweet.id, new_since_id)
+        if tweet.in_reply_to_status_id is not None:
+            continue
+        if any(keyword in tweet.text.lower() for keyword in keywords):
+            startDate = datetime.datetime(2020, 5, 19, 0, 0, 0)
+            if tweet.created_at > startDate:
+                if has_firm_friendship(api, tweet):
+                    print(f"Replying to {tweet.user.name}(@{tweet.user.screen_name}, {tweet.user.id_str}): ")
+                    print(f"\"{tweet.text}\"")
+                    api.update_status(
+                        status=f"Hello, {tweet.user.name}! I'm {MY_SCREEN_NAME}, Nice2meetu.",
+                        in_reply_to_status_id=tweet.id,
+                        auto_populate_reply_metadata="true"
+                    )
+                else:
+                    print(f"The user {tweet.user.name}(@{tweet.user.screen_name}, {tweet.user.id_str}) and you/me(@{MY_SCREEN_NAME}) seem to have no firm frindship...(They don't seem to be following each other.)")
+            #else:
+            #    print(f"Ignore: {tweet.text} from {tweet.user.name} cuz it's a old tweet...")
+    return new_since_id
+
+def main():
+    api = create_api()
+    since_id = 1
+    while True:
+        since_id = check_mentions(api, ["野獣先輩", "nnn1590", "help", "hi" ], since_id)
+        print("Waiting...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
